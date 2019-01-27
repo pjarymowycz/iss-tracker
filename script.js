3b6253b393162aa869179aa379745b0ccbb63ee4
pjarymowycz <42789243+pjarymowycz@users.noreply.github.com>
2019-01-27 16:03:21 -0500

    Leaflet simple map
    Uploaded files for simple Leaflet map.

---
+++ script.js
@@ -0,0 +1,17 @@
+// This isn't necessary but it keeps the editor from thinking L is a typo
+/* global L */
+
+var tileLink = 'https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png';
+
+var mapOptions = {
+  // dragging: false,
+  zoomDelta: 0.25,
+  inertiaDeceleration: 1
+};
+
+var map = L.map('map', mapOptions).setView([38.500893,-98.745117], 5);
+
+// Add base layer
+L.tileLayer(tileLink, {
+  maxZoom: 18
+}).addTo(map);
