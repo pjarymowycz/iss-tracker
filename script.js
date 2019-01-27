26cbbff0fa9af46f30ba0f8f0f65c790c841b755
pjarymowycz <42789243+pjarymowycz@users.noreply.github.com>
2019-01-27 16:24:38 -0500

    Add ArcGIS tile layer
    Added link to ArcGIS satellite tile layer.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // This isn't necessary but it keeps the editor from thinking L is a typo
 /* global L */
 
-var tileLink = 'https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png';
+// var tileLink = 'https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png';
+var tileLink = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{x}/{y}.jpg';
 
 var mapOptions = {
   // dragging: false,
